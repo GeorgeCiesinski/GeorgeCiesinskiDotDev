3f613bcf4c29ce6d181a004b91dd46cceeacdb5d
George Ciesinski <george.ciesin@gmail.com>
2026-08-18 23:26:35 -0400

    Changed about section and resume link placement.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -50,16 +50,19 @@ export function Home() {
               About
             </h2>
             <p>
-              I am an avid programmer who enjoys learning new things about
-              programming. I am most experienced in Javascript and Python, but I
-              also have experience with C#, HTML/CSS, SASS and SQL.
+              Hi there, I am a Full Stack Developer with experience building and maintaining enterprise web applications. I use React, TypeScript, and Node, but I also have experience with Python and C#. 
             </p>
-            <a
-              className="btn btn--primary"
-              href="/files/GeorgeCiesinskiResume.pdf"
-            >
-              Resume
-            </a>
+            <p>
+              Check out my resume and my personal projects below!
+            </p>
+            <div className="resume-link">
+              <a
+                className="btn btn--primary"
+                href="/files/GeorgeCiesinskiResume.pdf"
+              >
+                Resume
+              </a>
+            </div>
           </div>
         </div>
       </section>
